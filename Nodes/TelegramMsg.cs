fff6be36cd6f7b536a306be2e00c52f55182ceec
perschak.t <thomas.perschak@live.at>
2020-09-28 19:16:22 +0200

    use curl for https connection since monos TLS is outdated

--- Nodes/TelegramMsg.cs
+++ Nodes/TelegramMsg.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Text;
 using System.Linq;
 using System.Web;
+using System.Diagnostics;
 using Newtonsoft.Json;
 
 namespace teclab_at.logic.collection {
@@ -98,8 +99,13 @@ namespace teclab_at.logic.collection {
 
             // Schedule as async task ...
             this.SignalLogicError(false);
-            Task task = new Task(() => SendMessage(this, this.Message.Value, this.MessageNl.Value, this.SendRetries.Value));
-            task.Start();
+            if (Environment.OSVersion.Platform == PlatformID.Unix) {
+                Task task = new Task(() => SendMessage_curl(this, this.Message.Value, this.MessageNl.Value, this.SendRetries.Value));
+                task.Start();
+            } else {
+                Task task = new Task(() => SendMessage_net(this, this.Message.Value, this.MessageNl.Value, this.SendRetries.Value));
+                task.Start();
+            }
         }
 
         private class MsgResponse {
@@ -108,7 +114,69 @@ namespace teclab_at.logic.collection {
             public String description { get; set; } = String.Empty;
         }
 
-        static void SendMessage(TelegramMsg parent, String message, String messageNl, int sendRetries) {
+        static void SendMessage_curl(TelegramMsg parent, String message, String messageNl, int sendRetries) {
+            // Add a second line of message if given
+            if (messageNl != String.Empty) {message += Environment.NewLine + messageNl;}
+
+            int tryNr = 1;
+            while (sendRetries >= 0) {
+                try {
+                    parent.Log("SendMessage (try " + tryNr.ToString() + ") ...");
+                    parent.SendMessage_curlworker(message);
+                    parent.Log("SendMessage (try " + tryNr.ToString() + ") success");
+                    break;
+                } catch (Exception ex) {
+                    parent.SignalLogicError();
+                    parent.Log("SendMessage (try " + tryNr.ToString() + ") failed: " + ex.ToString(), true);
+                }
+                // Sleep for 50 seconds. Note that the curl timeout is set to 10 seconds which gives us a one minute cycle
+                Thread.Sleep(10000);
+                sendRetries -= 1;
+                tryNr += 1;
+            }
+        }
+
+        void SendMessage_curlworker(String message) {
+            // Create the payload
+            var payload = new NameValueCollection();
+            payload.Add("chat_id", this.ChatId.Value);
+            payload.Add("text", HttpUtility.UrlEncode(message, Encoding.UTF8));
+
+            // Build the complete URL
+            String msgUrl = String.Format(TelegramMsg.webUrl, this.BotToken.Value) + "?" + this.ToKeyValuePairs(payload);
+            this.Log(msgUrl);
+
+            // create and define the process
+            Process curlCmd = new Process();
+            curlCmd.StartInfo.FileName = "/usr/bin/curl";
+            curlCmd.StartInfo.Arguments = string.Format("-m 10 \"{0}\" ", msgUrl);
+            curlCmd.StartInfo.RedirectStandardOutput = true;
+            curlCmd.StartInfo.RedirectStandardError = true;
+            curlCmd.StartInfo.CreateNoWindow = true;
+            curlCmd.StartInfo.UseShellExecute = false;
+
+            // Execute and wait until it exits
+            curlCmd.Start();
+            if (!curlCmd.WaitForExit(15000)) {
+                curlCmd.Kill();
+                throw new System.Exception("Process did not exit in time");
+            } else if (curlCmd.ExitCode != 0) {
+                String data = curlCmd.StandardError.ReadToEnd();
+                throw new System.Exception("Process error: " + data);
+            } else {
+                String data = curlCmd.StandardOutput.ReadToEnd();
+                if (data == String.Empty) {throw new System.Exception("Process returned empty result");}
+                this.Log(data);
+                // Parse the JSON string into the response class
+                MsgResponse response = JsonConvert.DeserializeObject<MsgResponse>(data);
+                // Check
+                if (response.ok == false) {
+                    throw new System.Exception(data);
+                }
+            }
+        }
+
+        static void SendMessage_net(TelegramMsg parent, String message, String messageNl, int sendRetries) {
             // Add a second line of message if given
             if (messageNl != String.Empty) {message += Environment.NewLine + messageNl;}
 
@@ -124,6 +192,7 @@ namespace teclab_at.logic.collection {
             // Create the HTTP request using the GET method and the required headers
             HttpWebRequest httpRequest = (HttpWebRequest)HttpWebRequest.Create(msgUrl);
             httpRequest.Method = "GET";
+            httpRequest.AuthenticationLevel = AuthenticationLevel.None;
 
             Boolean sendSuccess = false;
             int tryNr = 1;
@@ -146,7 +215,11 @@ namespace teclab_at.logic.collection {
             try {
                 if (!sendSuccess) {
                     parent.Log("SendMessage (try " + tryNr.ToString() + ") ...");
+                    ServicePointManager.ServerCertificateValidationCallback =
+                        delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+                    parent.Log("A");
                     httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+                    parent.Log("B");
                 }
                 Stream streamIn = httpResponse.GetResponseStream();
                 var streamReader = new StreamReader(streamIn, Encoding.UTF8);
@@ -166,19 +239,21 @@ namespace teclab_at.logic.collection {
                 streamIn.Close();
                 httpResponse.Close();
             } catch (WebException ex) {
-                httpResponse = (HttpWebResponse)ex.Response;
-                Stream streamIn = httpResponse.GetResponseStream();
-                var streamReader = new StreamReader(streamIn, Encoding.UTF8);
-                String data = streamReader.ReadToEnd();
-                parent.Log(data);
-                parent.SignalLogicError();
-                // Cleanup
-                streamReader.Close();
-                streamIn.Close();
-                httpResponse.Close();
-                // Notify the upper logic that something went wrong
                 parent.SignalLogicError();
                 parent.Log("SendMessage (try " + tryNr.ToString() + ") failed: " + ex.ToString(), true);
+                try {
+                    httpResponse = (HttpWebResponse)ex.Response;
+                    Stream streamIn = httpResponse.GetResponseStream();
+                    var streamReader = new StreamReader(streamIn, Encoding.UTF8);
+                    String data = streamReader.ReadToEnd();
+                    parent.Log(data);
+                    // Cleanup
+                    streamReader.Close();
+                    streamIn.Close();
+                    httpResponse.Close();
+                } catch (Exception exd) {
+                    parent.Log("SendMessage (try " + tryNr.ToString() + ") failed fatal: " + exd.ToString(), true);
+                }
             }
         }
     }
